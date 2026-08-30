09d77bbf76afd78d7c973890300803dc44839e2e
aiautotool <161418348+aiautotool@users.noreply.github.com>
2026-08-31 01:35:05 +0700

    feat(image-editor): export editor SDK surface

---
+++ packages/image-editor/src/index.ts
@@ -0,0 +1,6 @@
+export * from './types';
+export * from './EditSession';
+export * from './EditValidator';
+export * from './ImageEditorEngine';
+export * from './CallbackRendererAdapter';
+export * from './presets';
