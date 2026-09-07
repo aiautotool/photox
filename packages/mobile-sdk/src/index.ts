4aedb28e9233ad5c242041a7e7e18564d34205fe
aiautotool <161418348+aiautotool@users.noreply.github.com>
2026-09-07 15:50:03 +0700

    feat(v4): export resumable mobile upload client

--- packages/mobile-sdk/src/index.ts
+++ packages/mobile-sdk/src/index.ts
@@ -2,6 +2,7 @@ export * from './DesktopClient';
 export * from './MediaApiClient';
 export * from './PairingStore';
 export * from './PhotoXMobileSDK';
+export * from './ResumableUploadClient';
 export * from './media/MediaActions';
 export * from './media/ExpoMediaLibraryAdapter';
 export * from './media/FunctionMediaEditorAdapter';
