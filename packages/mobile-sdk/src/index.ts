d3f2d944aa0d2e3614997df8221056be86119178
aiautotool <161418348+aiautotool@users.noreply.github.com>
2026-09-07 18:49:25 +0700

    feat(v4): export mobile upload transport policy

--- packages/mobile-sdk/src/index.ts
+++ packages/mobile-sdk/src/index.ts
@@ -3,6 +3,7 @@ export * from './MediaApiClient';
 export * from './PairingStore';
 export * from './PhotoXMobileSDK';
 export * from './ResumableUploadClient';
+export * from './UploadTransportPolicy';
 export * from './media/MediaActions';
 export * from './media/ExpoMediaLibraryAdapter';
 export * from './media/FunctionMediaEditorAdapter';
